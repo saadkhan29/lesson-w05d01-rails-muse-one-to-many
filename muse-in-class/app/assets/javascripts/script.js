a9938f18cbd7e6e9181b2c20b0a8e4c8e4a993b3
Marc Wright <marc.wright@gmail.com>
2019-07-16 16:22:53 +0300

    Add spinner and jquery fade in to table

--- muse-in-class/app/assets/javascripts/script.js
+++ muse-in-class/app/assets/javascripts/script.js
@@ -1,3 +1,13 @@
+
 $(() => {
   console.log('ready!');
+  
+
+  setTimeout(function(){ 
+    $('.artists-table').fadeIn();
+    $('.spinner-border').hide()
+  }, 1000);
+
+
+  // setTimeout(function(){ alert("Hello"); }, 3000);
 })
